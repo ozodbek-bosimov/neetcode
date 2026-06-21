3fafdf2481bb203b6a755cc625322bea32a4b3b4
Ozodbek Bosimov <bosimovozodbekdev@gmail.com>
2026-06-21 12:19:14 +0500

    Add: subtree-of-a-binary-tree - submission-0

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-0.py
@@ -0,0 +1,23 @@
+# n and m are the number of nodes in root and subRoot
+# T: O(m * n)
+# S: O(n + m)
+class Solution:   
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        def isSame(currRoot, subRoot):
+            if currRoot is None and subRoot is None:
+                return True
+            if currRoot is None or subRoot is None:
+                return False
+            if currRoot.val != subRoot.val:
+                return False
+            
+            return isSame(currRoot.left, subRoot.left) and isSame(currRoot.right, subRoot.right)
+        
+        def dfs(node):
+            if node is None:
+                return False
+            if isSame(node, subRoot):
+                return True
+            return dfs(node.left) or dfs(node.right)
+        
+        return dfs(root)
