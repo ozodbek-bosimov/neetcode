31f87dd14b1503e2fa8a4fea7777c220ad5ae203
Ozodbek Bosimov <bosimovozodbekdev@gmail.com>
2026-07-17 01:07:35 +0500

    Add: design-hashset - submission-2

---
+++ Data Structures & Algorithms/design-hashset/submission-2.py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self, val, next = None):
+        self.val = val
+        self.next = next
+
+class MyHashSet:
+    def __init__(self):
+        self.size = 10_007
+        self.hashset = [Node(0) for _ in range(self.size)]
+
+    def _get_hash(self,num)-> int:
+        return num % self.size
+
+    def add(self, key: int) -> None:
+        k = self._get_hash(key)
+        node = self.hashset[k]
+        while node.next:
+            if node.next.val == key:
+                return
+            node = node.next
+        node.next = Node(key)
+
+    def remove(self, key: int) -> None:
+        k = self._get_hash(key)
+        node = self.hashset[k]
+        while node.next:
+            if node.next.val == key:
+                node.next = node.next.next
+                return
+
+    def contains(self, key: int) -> bool:
+        k = self._get_hash(key)
+        node = self.hashset[k]
+        while node.next:
+            if node.next.val == key:
+                return True
+            node = node.next
+        return False
+
+        
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
